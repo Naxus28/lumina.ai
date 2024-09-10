2d18024564de27e89c7b1aa5740ad0bc7ce31aac
naxus28 <gabrielferraz27@gmail.com>
2024-09-09 23:24:18 -0400

    Changes tagline

--- app/components/landing/hero/HeroSection.tsx
+++ app/components/landing/hero/HeroSection.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 export const HeroSection: React.FC = () => (
 	<section className="py-20 mt-10 px-4 sm:px-6 lg:px-8 bg-gradient-to-b from-white to-[#dae1e8]">
 		<div className="max-w-4xl mx-auto text-center">
-			<h1 className="text-4xl sm:text-4xl font-bold text-gray-900 mb-6">Bright Academic Careers, Brilliantly Presented</h1>
+			<h1 className="text-4xl sm:text-4xl font-bold text-gray-900 mb-6">Bright Careers, Brilliantly Presented</h1>
 			<p className="text-xl text-gray-600 mb-8">
 				Lumina revolutionizes academic job applications with AI-powered precision. From recent PhD graduates to postdoctoral researchers and experienced professors, we transform your achievements into compelling narratives. Our platform crafts application materials that capture hiring committees' attention and distinguish you in a competitive field. By streamlining the application process, Lumina saves you valuable time, allowing you to focus on your groundbreaking research and inspiring
 				teaching.
